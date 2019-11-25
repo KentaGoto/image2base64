872b7cb8b5cfcf250e14965dc3d853e598dd75fc
KentaGoto <gotokentag@gmail.com>
2019-11-25 17:50:26 +0900

    3rd

--- image2base64.pl
+++ image2base64.pl
@@ -8,14 +8,12 @@ use MIME::Base64;
 
 ##############################################################
 ### jpeg、png、gif、bmpをbase64に変換する。
-### HTMLに埋め込むことを想定している。
-### https://gist.github.com/tatesuke/57054a31e193ac835b60
 ##############################################################
 
 # クリップボードから対象ファイルを得る
-my $file_tmp = Win32::Clipboard();
+my $file_tmp              = Win32::Clipboard();
 my $file_tmp_decode_paste = $file_tmp->GetAs(CF_UNICODETEXT);
-my $file  = decode('UTF16-LE', $file_tmp_decode_paste);
+my $file                  = decode( 'UTF16-LE', $file_tmp_decode_paste );
 $file =~ s{^"}{};
 $file =~ s{"$}{};
 
@@ -28,24 +26,18 @@ my $mime_type;
 
 if ( $file =~ $regex_extension_jpg ) {
     $mime_type = 'jpeg';
-}
-elsif ( $file =~ $regex_extension_png ) {
+} elsif ( $file =~ $regex_extension_png ) {
     $mime_type = 'png';
-}
-elsif ( $file =~ $regex_extension_gif ) {
+} elsif ( $file =~ $regex_extension_gif ) {
     $mime_type = 'gif';
-}
-elsif ( $file =~ $regex_extension_bmp ) {
+} elsif ( $file =~ $regex_extension_bmp ) {
     $mime_type = 'bmp';
-}
-else {
+} else {
     print $file, "\n";
     die 'Unknown image file type.';
 }
 
-my $filesize = -s $file;    # バイト数を得る
-
-#print $filesize . "\n";
+my $filesize = -s $file; # バイト数を得る
 
 open my $IN, '<', $file;
 binmode $IN;
@@ -59,8 +51,6 @@ close $IN;
 # バイナリをbase64にエンコード
 my $base64 = encode_base64( $binary, '' );
 
-#print 'data:image/' . $mime_type . ';base64,', $base64;
 my $toClipboard = 'data:image/' . $mime_type . ';base64,' . $base64;
-#print $toClipboard;
 
 Clipboard->copy($toClipboard);
